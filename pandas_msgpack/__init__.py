b2aa61383eea1e2794b01a5d222ea46a65a302e3
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-03-19 12:55:02 +0100

    remove distutils for checking pandas version (deprecated in newer {ython versions)

--- pandas_msgpack/__init__.py
+++ pandas_msgpack/__init__.py
@@ -3,12 +3,12 @@
 # pandas versioning
 import pandas
 
-from distutils.version import LooseVersion
-pv = LooseVersion(pandas.__version__)
+# from distutils.version import LooseVersion
+# pv = LooseVersion(pandas.__version__)
 
-if pv < '0.19.0':
-    raise ValueError("pandas_msgpack requires at least pandas 0.19.0")
-_is_pandas_legacy_version = pv.version[1] == 19 and len(pv.version) == 3
+# if pv < '0.19.0':
+#     raise ValueError("pandas_msgpack requires at least pandas 0.19.0")
+# _is_pandas_legacy_version = pv.version[1] == 19 and len(pv.version) == 3
 
 from .packers import to_msgpack, read_msgpack
 
@@ -20,4 +20,5 @@ from ._version import get_versions
 versions = get_versions()
 __version__ = versions.get('closest-tag', versions['version'])
 __git_revision__ = versions['full-revisionid']
-del get_versions, versions, pv, LooseVersion, pandas
+# del get_versions, versions, pv, LooseVersion, pandas
+del get_versions, versions, pandas
