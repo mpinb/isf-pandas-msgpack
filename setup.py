eefdcae08e0e430f1d7251fbd277733edc3e2c5b
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-05-12 15:33:15 +0200

    Update cython extension locations

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ extra_compile_args = [] if IS_WINDOWS else ["-Wno-unused-function", "-std=c++11"
 extensions = [
     Extension(
         "isf_pandas_msgpack.msgpack._packer",
-        sources=["src/msgpack/_packer.pyx"],
+        sources=["src/isf_pandas_msgpack/msgpack/_packer.pyx"],
         language="c++",
-        include_dirs=["src/includes"],
+        include_dirs=["src/isf_pandas_msgpack/includes"],
         extra_compile_args=extra_compile_args,
     ),
     Extension(
         "isf_pandas_msgpack.msgpack._unpacker",
-        sources=["src/msgpack/_unpacker.pyx"],
+        sources=["src/isf_pandas_msgpack/msgpack/_unpacker.pyx"],
         language="c++",
-        include_dirs=["src/includes"],
+        include_dirs=["src/isf_pandas_msgpack/includes"],
         extra_compile_args=extra_compile_args,
     ),
     Extension(
         "isf_pandas_msgpack._move",
-        sources=["src/move.c"]
+        sources=["src/isf_pandas_msgpack/move.c"]
     ),
 ]
 
